e88740e6d5a7096e1b5e634f9c2bc16db959ffe6
Isaac Vieira <isaacvf.dev@gmail.com>
2024-03-26 16:40:49 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# Scrimba - VanLife project
+
+This is a project of the [VanLife on Scrimba](https://scrimba.com/learn/frontend). Scrimba helps you improve your coding skills by building realistic projects. This section is all about React router.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Requirements](#requirements)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [Getting Started with Create React App](#getting-started-with-create-react-app)
+- [My process](#my-process)
+  - [Built with](#built-with)  
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### Requirements
+
+- Routing Basics
+- Navigation
+- Layout and Index Routes
+- Nested Routes
+- Route and Search Parameters
+- Protected Routes
+- +
+  
+
+
+### Screenshot
+
+![screenshot](/public/screenshoot.png)
+
+### Links
+
+- Live Site URL: [@Netlify](https://component-liby.netlify.app/)
+- Solution URL: [@GitHub](https://github.com/Isaacvf-dev/Component-Library)
+
+### Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## My process
+
+### Built with
+
+- HTML
+- CSS 
+- React.JS
+
+  
+
+
+
+### Useful resources
+
+- [ReactJS](https://reactjs.org/tutorial/tutorial.html) - How to set up a local development environment on your computer
+  
+
+
+## Author
+
+<table>
+  <tr>    
+    <td align="center">
+      <a href="https://github.com/Isaacvf-dev">
+        <img src="https://avatars.githubusercontent.com/u/123469000?v=4" width="100px;" alt="Foto do Isaac Vieira no GitHub"/><br>
+        <sub>
+          <b>Isaac Vieira</b>
+        </sub>
+      </a>
+    </td>
+  </tr>
+</table>
---
+++ api.js
@@ -0,0 +1,75 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, doc, getDoc, query, where } from "firebase/firestore/lite"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCAGbYTLurxIz3uV8tscYY1Yl4TaY3gk2o",
+  authDomain: "vanlife-ad52a.firebaseapp.com",
+  projectId: "vanlife-ad52a",
+  storageBucket: "vanlife-ad52a.appspot.com",
+  messagingSenderId: "262874125898",
+  appId: "1:262874125898:web:9aacc2c43d0e56ec8838b3"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+const vansCollectionRef = collection(db, "vans")
+
+export async function getVans() {
+    const snapshot = await getDocs(vansCollectionRef)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
+}
+
+export async function getVan(id) {
+    const docRef = doc(db, "vans", id)
+    const snapshot = await getDoc(docRef)
+    return {
+        ...snapshot.data(),
+        id: snapshot.id
+    }
+}
+
+export async function getHostVans(id) {
+    const q = query(vansCollectionRef, where("hostId", "==", "123"))
+    const snapshot = await getDocs(q)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
+}
+
+// export async function getHostVans(id) {
+//     const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
+//     const res = await fetch(url)
+//     if (!res.ok) {
+//         throw {
+//             message: "Failed to fetch vans",
+//             statusText: res.statusText,
+//             status: res.status
+//         }
+//     }
+//     const data = await res.json()
+//     return data.vans
+// }
+
+export async function loginUser(creds) {
+    const res = await fetch("/api/login",
+        { method: "post", body: JSON.stringify(creds) }
+    )
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw {
+            message: data.message,
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+
+    return data
+}
